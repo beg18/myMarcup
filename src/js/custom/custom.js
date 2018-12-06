c192e03d64bb0667b5ee7b3a20f562d7b2360cf2
Alibekov Khiyirbeg <beg_geb@mail.ru>
2018-12-06 22:20:11 +0300

    Изменения в проекте

---
+++ src/js/custom/custom.js
@@ -0,0 +1,21 @@
+$(function(){
+
+    $('nav').coreNavigation({
+        menuPosition: "center",
+        container: true,
+        dropdownEvent: 'accordion',
+        onOpenDropdown: function(){
+            console.log('open');
+        },
+        onCloseDropdown: function(){
+            console.log('close');
+        }
+    });
+
+    $("#js-lightgallery").lightGallery({
+        mode: "lg-slide-skew-rev",
+        download: false,
+        thumbContHeight: 50
+    });
+
+});
---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/* Libs
+===========================*/
+//= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../node_modules/corenav/coreNavigation.js
+//= ../../bower_components/lightgallery/dist/js/lightgallery.min.js
+//= ../../bower_components/lightgallery/dist/js/lg-thumbnail.min.js
+//= ../../bower_components/lightgallery/dist/js/lg-fullscreen.min.js
+
+/* Custom
+===========================*/
+//= custom/custom.js
+
